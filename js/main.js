dfcc17a0a3fa0132126b9e980c1fa40fe577713a
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-02-21 16:32:09 +0100

    Search planet works, render it wip

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const API_URL = 'https://majazocom.github.io/Data/solaris.json';
 
+
 getPlanets();
 
 function getPlanets() {
     fetch(API_URL)
     .then(respons => respons.json())
-    .then(data => renderPlanetsToUI(data));
+    .then(data => {
+        renderPlanetsToUI(data);
+        searchPlanet(data);
+    });
 }
 
 function renderPlanetsToUI(planets) {
@@ -24,5 +28,53 @@ function renderPlanetsToUI(planets) {
 function onPlanetClick(element, item) {
     element.addEventListener('click', () => {
         console.log(`Wow ${item.name}`);
+        try {
+            renderSelectedPlanetToUI();
+        } catch (e) {
+            console.log(e);
+        }
     });
+}
+
+function searchPlanet(planets) {
+    let searchInputEl = document.querySelector('#search-input');
+    let searchButton = document.querySelector('#search-btn');
+
+    searchButton.addEventListener('click', () => {
+        let showResultEl = document.querySelector('.show-result');
+        let searchInput = searchInputEl.value;
+        console.log(searchInput);
+
+        let planetIndex = planets.findIndex(planet => planet.name.toLowerCase() === searchInput.toLowerCase()); 
+        try {
+            showResultEl.style.color = 'white';
+            showResultEl.href = 'planet-info.html';
+            showResultEl.innerHTML = `Take me to: ${planets[planetIndex].name}`;
+            localStorage.setItem('currentPlanet', JSON.stringify(planets[planetIndex]));
+            onPlanetClick(showResultEl);
+        } catch (e) {
+            console.log(e);
+            showResultEl.style.color = 'red';
+            showResultEl.innerHTML = `That is not a planet :( <br>
+                Try again`;
+        } 
+    });
+}
+
+
+
+
+function renderSelectedPlanetToUI() {
+    let planetNameEl = document.querySelector('.planet--name');
+    let planetLatinNameEl = document.querySelector('.planet--latin-name');
+    let planetDescriptionEl = document.querySelector('.planet--description');
+    let planetCircumferenceEl = document.querySelector('.planet--circumference');
+    let planetDistanceEl = document.querySelector('.planet--distance');
+    let planetMaxTempEl = document.querySelector('.planet--max-temp');
+    let planetMinTempEl = document.querySelector('.planet--min-temp');
+    let planetMoonsEl = document.querySelector('.planet--moons');
+
+    let planetToShow = JSON.parse(localStorage.getItem('currentPlanet'));
+    console.log(planetToShow);
+    planetNameEl.innerHTML = `${planetToShow.name}`;
 }
